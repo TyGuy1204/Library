a2564344143be25a29e7f62c874d2b629660b9c7
Tyler Scott <tylerclaytonscott@gmail.com>
2025-01-16 12:25:37 -0500

    Add functionality to take user input and add it to library

--- Library.js
+++ Library.js
@@ -1,5 +1,22 @@
 const myLibrary = [];
 
+
+document.addEventListener("DOMContentLoaded", () => {
+    let button = document.querySelector("button");
+    button.addEventListener("click", () => {
+        let title = document.querySelector('#title').value;
+        let author = document.querySelector('#author').value;
+        let pages = document.querySelector('#pages').value;
+        let read = document.querySelector('#read').value;
+        let book = new Book(title, author, pages, read);
+        addBookToLibrary(book);
+        console.log(myLibrary);
+        console.log(book);
+    }); 
+});
+
+
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
