93645ab778832a2a4b3fd51a7863a653474c7486
Tyler Scott <tylerclaytonscott@gmail.com>
2025-01-17 10:43:12 -0500

    Move toggleReadStatus to book class

--- Library.js
+++ Library.js
@@ -1,16 +1,14 @@
 const myLibrary = [];
 
-class library{
-    addBookToLibrary(book){
-        
-    }
-}
-class book{
+class Book{
     constructor(title,author,pages,read){
-        this.title  = title;
-        this.author  = author;
-        this.pages  = pages;
-        this.read  = read;
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+    toggleReadStatus(){
+        this.read = !this.read;
     }
 }
 
@@ -51,6 +49,13 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
